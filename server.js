9492069362d0ace4954d1eb1aeae85aab37a4f28
Benyamin Lea <lea.benyamin@student-cs.fr>
2021-06-10 07:55:59 +0200

    Route to get the rankings and route to post a score

--- server.js
+++ server.js
@@ -5,71 +5,41 @@ var cors = require("cors");
 
 require("dotenv").config();
 
-process.env.GOOGLE_APPLICATION_CREDENTIALS = "./token.json";
-const projectId = "robust-seat-301020";
-const location = "global";
-
-const { TranslationServiceClient } = require("@google-cloud/translate");
-const translationClient = new TranslationServiceClient();
-
-async function translateText(text) {
-  const request = {
-    parent: `projects/${projectId}/locations/${location}`,
-    contents: [text],
-    mimeType: "text/plain",
-    sourceLanguageCode: "fr",
-    targetLanguageCode: "en",
-  };
-  try {
-    const [response] = await translationClient.translateText(request);
-    return response.translations[0].translatedText;
-  } catch (error) {
-    console.error(error.details);
-  }
-}
-
 const { MongoClient } = require("mongodb");
 const url = process.env.URL;
 const client = new MongoClient(url, { useUnifiedTopology: true });
-const dbName = "words";
-let words_collection = "";
-let numberOfWords = 0;
+const dbName = "rankings";
 
 client.connect().then((response) => {
   if (response.topology.s.state) {
     console.log("Status: " + response.topology.s.state);
     const db = client.db(dbName);
-    words_collection = db.collection("words");
-    all_words = words_collection
-      .find()
-      .toArray()
-      .then((words) => {
-        numberOfWords = words.length;
-      });
+    rankings = db.collection("rankings");
+    all_rankings = rankings.find().toArray();
   } else {
     console.log("Problem connecting to MongoDB");
   }
 });
 
-const getRandomNumber = () => {
-  let id = Math.floor(Math.random() * numberOfWords) + 1;
-  return id;
-};
-
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  const random = getRandomNumber();
-  words_collection
-    .findOne({
-      _id: random,
-    })
-    .then((word) => {
-      translateText(word.french).then((english) => {
-        res.json({ french: word.french, english: english });
-      });
-    });
+  rankings.find().toArray();
+});
+
+app.get("/top10", (req, res) => {
+  rankings
+    .find({})
+    .sort({ averageScore: -1 })
+    .limit(10)
+    .then((ranks) => res.json(ranks));
+});
+
+app.post("/score", async (req, res) => {
+  const { Score, Username } = req.body;
+  await rankings.insert({ Username, Score });
+  res.send("Score saved");
 });
 
 app.listen(port, () => {
